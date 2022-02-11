3dcd4d468e84aa0775c55f26f96a9f2569f63b59
Afiss dia <afissdia07@gmail.com>
2022-02-11 23:06:58 +0000

    mon premier commit

---
+++ src/main/java/com/dia/student/GestionStudentApplication.java
@@ -0,0 +1,13 @@
+package com.dia.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionStudentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dia/student/GestionStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dia.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
